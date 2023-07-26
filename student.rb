247126479bc7850f1778ad28099a2f700bbc85d3
Pablo <ezekielgonzalezr@gmail.com>
2023-07-26 16:06:53 -0600

    Add classroom method to student class

--- student.rb
+++ student.rb
@@ -1,6 +1,11 @@
 require './person'
+require './classroom'
+
 
 class Student < Person
+
+attr_accessor :clasroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @clasroom = classroom
@@ -9,4 +14,11 @@ class Student < Person
   def play_hooky
     '¯\(ツ)/¯'
   end
+
+  def classroom=(classroom)
+    @clasroom = classroom
+    classroom.students << self unless clasroom.students.include?(self)
+  end
 end
+
+# student = Student.new('g1', 22, 'max')
