417e89f6e80d82b4202f86e42e2d5732c656a454
Pablo <ezekielgonzalezr@gmail.com>
2023-07-27 21:25:57 -0600

    Set up static atribute for book

--- book.rb
+++ book.rb
@@ -3,14 +3,19 @@ require './rentals'
 class Book
   attr_accessor :title, :author
   attr_reader :rentals
-
+  @@books = []
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
+    @@books.push(self)
   end
 
   def add_rental(date, person)
     Rental.new(date, self, person)
+  end
+
+  def self.getBooks
+    @@books
   end
 end
