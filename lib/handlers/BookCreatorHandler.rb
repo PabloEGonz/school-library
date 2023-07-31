a279a742020aa33fadefe1cd8e5fda2636e36f2d
Jrmcuervo <jrmcuervom@gmail.com>
2023-07-31 14:25:45 -0600

    Fix linters

--- lib/handlers/BookCreatorHandler.rb
+++ lib/handlers/BookCreatorHandler.rb
@@ -1,7 +1,7 @@
 require_relative '../models/Book'
 
 class BookCreator
-    def create_book(title, author)
+  def create_book(title, author)
     Book.new(title, author)
-    end
+  end
 end
--- lib/handlers/BookListHandler.rb
+++ lib/handlers/BookListHandler.rb
@@ -1,21 +1,21 @@
 class BookList
-    def initialize(array)
-        @books = array
+  def initialize(array)
+    @books = array
+  end
+
+  def list_all_books
+    @books.map { |_book| puts book_info(_book) }
+  end
+
+  def list_book_with_idx
+    @books.map.with_index do |book, i|
+      puts "(#{i + 1}) #{book_info(book)}"
     end
-    
-    def list_all_books
-        @books.map { |_book| puts book_info(_book) }
-      end
-    
-      def list_book_with_idx
-        @books.map.with_index do |book, i|
-          puts "(#{i + 1}) #{book_info(book)}"
-        end
-      end
-    
-      private
-    
-      def book_info(book)
-        " - #{book.title} by #{book.author}"
-      end
+  end
+
+  private
+
+  def book_info(book)
+    " - #{book.title} by #{book.author}"
+  end
 end
