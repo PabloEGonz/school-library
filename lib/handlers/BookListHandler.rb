aa2ac91959a8e9eb681085de382b619927c490e8
Pablo <ezekielgonzalezr@gmail.com>
2023-07-31 13:35:00 -0600

    Add list handler class

--- lib/handlers/BookListHandler.rb
+++ lib/handlers/BookListHandler.rb
@@ -1,1 +1,21 @@
+class BookList
+    def initialize(array)
+        @books = array
+    end
 
+    def 
+        @books.map { |_book| puts book_info }
+      end
+    
+      def list_book_with_idx
+        @books.map.with_index do |book, i|
+          puts "(#{i + 1}) #{book_info(book)}"
+        end
+      end
+    
+      private
+    
+      def book_info(book)
+        " - #{book.title} by #{book.author}"
+      end
+end
