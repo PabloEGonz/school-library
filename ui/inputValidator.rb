b1bc1c1401acc67abe6a4a01d69b73cdcce648ee
Pablo <ezekielgonzalezr@gmail.com>
2023-07-31 18:07:54 -0600

    Add input validator class

---
+++ ui/inputValidator.rb
@@ -0,0 +1,14 @@
+class Validator
+    def boolean(input)
+        /^[YyNn]$/.match?(input)
+    end
+
+    def number(input)
+        /^\d+$/.match?(input)
+    end
+
+    def date(input)
+        /^\d{4}-\d{2}-\d{2}$/.math?(input)
+    end
+end
+ 
