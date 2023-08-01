603f0ce9860b1c90648151910acc6ca97908df76
Pablo <ezekielgonzalezr@gmail.com>
2023-08-01 07:55:39 -0600

    Add rentals as optional param

--- lib/handlers/BookCreatorHandler.rb
+++ lib/handlers/BookCreatorHandler.rb
@@ -1,7 +1,7 @@
 require_relative '../models/Book'
 
 class BookCreator
-  def create_book(title, author)
-    Book.new(title, author)
+  def create_book(title, author, rentals: [])
+    Book.new(title, author, rentals)
   end
 end
