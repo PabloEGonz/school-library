2fea08f375b5b51d664726614fcd6302f72ddd64
Pablo <ezekielgonzalezr@gmail.com>
2023-08-02 11:18:05 -0600

    Add test for trimmer decorator

--- spec/trimmer_decorator_spec.rb
+++ spec/trimmer_decorator_spec.rb
@@ -1,3 +1,20 @@
-require 'rspec'
-
 require_relative 'spec_helper'
+
+describe TrimmerDecorator do
+  context 'Trimer is working as expected' do
+    it 'Trims names longer than 10 chars' do
+      trimmer = TrimmerDecorator.new('Ezequielpablo')
+
+      result = trimmer.correct_name
+
+      expect(result).to eq 'Ezequielpa'
+    end
+    it 'Names no longeer than 10 chars stay the same' do
+      trimmer = TrimmerDecorator.new('Pablo')
+
+      result = trimmer.correct_name
+
+      expect(result).to eq 'Pablo'
+    end
+  end
+end
