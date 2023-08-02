32e4a152d8df0d33345c9e7c83906c43c044a023
Pablo <ezekielgonzalezr@gmail.com>
2023-08-02 11:25:58 -0600

    Remove indentation

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,20 +1,20 @@
 require_relative 'spec_helper'
 
 describe CapitalizeDecorator do
-    context 'Capitalization is working as expected' do
-      it 'Capitalizes the first letter' do
-        name = CapitalizeDecorator.new('pablo')
-  
-        result = name.correct_name
-  
-        expect(result).to eq 'Pablo'
-      end
-      it 'Names already capitalized stay the same' do
-        name = CapitalizeDecorator.new('Eze')
-  
-        result = name.correct_name
-  
-        expect(result).to eq 'Eze'
-      end
+  context 'Capitalization is working as expected' do
+    it 'Capitalizes the first letter' do
+      name = CapitalizeDecorator.new('pablo')
+
+      result = name.correct_name
+
+      expect(result).to eq 'Pablo'
+    end
+    it 'Names already capitalized stay the same' do
+      name = CapitalizeDecorator.new('Eze')
+
+      result = name.correct_name
+
+      expect(result).to eq 'Eze'
     end
   end
+end
