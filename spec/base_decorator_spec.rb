35e0c3e7c357e1195b5840bb5b20e221f3d74e73
Pablo <ezekielgonzalezr@gmail.com>
2023-08-02 12:04:46 -0600

    Add test for base decorator

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -1,3 +1,11 @@
-require 'rspec'
-
 require_relative 'spec_helper'
+
+describe Decorator do
+  context 'Decorator is without any other class' do
+    it 'It raises error' do
+      name = Decorator.new('pablo')
+
+      expect { name.correct_name }.to raise_error(NotImplementedError)
+    end
+  end
+end
