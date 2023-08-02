f11fb7a4c2387e54062845cdf00b1da2373a5e72
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-02 14:10:23 -0600

    Create rental unit test

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,3 +1,23 @@
-require 'rspec'
-
 require_relative 'spec_helper'
+
+describe Rental do
+    before(:each) do
+      @book = Book.new("Test book", "Test author")
+      @person = Person.new(20, name: "Test person") 
+      @rental = Rental.new(Date.today, @book, @person)
+    end
+  
+    it "initializes with correct attributes" do
+      expect(@rental.date).to eq(Date.today)
+      expect(@rental.book).to eq(@book)
+      expect(@rental.person).to eq(@person)
+    end
+  
+    it "adds rental to the book's rentals" do
+      expect(@book.rentals).to include(@rental)
+    end
+  
+    it "adds rental to the person's rentals" do
+      expect(@person.rentals).to include(@rental)
+    end
+  end
