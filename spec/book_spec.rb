8cd81addcdebc0fd7f09a49968d92bb40e9eeb82
Pablo <ezekielgonzalezr@gmail.com>
2023-08-02 12:22:35 -0600

    Add more tests to book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -15,14 +15,34 @@ describe Book do
   end
 
   context 'Adding a rental to the existing book' do
-    it 'Create a rental' do
+    it 'Creates a rental and access the rental date' do
       book = Book.new('Got', 'George')
-      person = Student.new(id: 1, age: 15, name: 'Max', parent_permission: false)
+      person = Student.new(id: 1, age: 15, name: 'Max', parent_permission: true)
       date = '2020-12-12'
 
       book.add_rental(date, person)
 
       expect(book.rentals[0].date).to eq '2020-12-12'
+    end
+
+    it 'Creates a rental and access the person name' do
+      book = Book.new('Got', 'George')
+      person = Student.new(id: 100, age: 25, name: 'Mary', parent_permission: true)
+      date = '2022-10-10'
+
+      book.add_rental(date, person)
+
+      expect(book.rentals[0].person.name).to eq 'Mary'
+    end
+
+    it 'Creates a rental and access the person name' do
+      book = Book.new('Got', 'George')
+      person = Student.new(id: 10, age: 30, name: 'Larry', parent_permission: false)
+      date = '2023-08-10'
+
+      book.add_rental(date, person)
+
+      expect(book.rentals[0].book.author).to eq 'George'
     end
   end
 end
