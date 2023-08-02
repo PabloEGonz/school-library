af690ec3327675151093d3779af1493ca92b3f5d
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-02 14:08:25 -0600

    Create unit test for classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,3 +1,23 @@
-require 'rspec'
-
 require_relative 'spec_helper'
+
+describe Classroom do
+    before(:each) do
+      @classroom = Classroom.new("Test room")
+      @student = Student.new(id: 1, age: 20, name: "Test student")
+    end
+  
+    it "initializes with correct attributes" do
+      expect(@classroom.label).to eq("Test room")
+      expect(@classroom.students).to eq([])
+    end
+  
+    it "adds student to the classroom's students" do
+      @classroom.add_student(@student)
+      expect(@classroom.students).to include(@student)
+    end
+  
+    it "assigns the classroom to the added student" do
+      @classroom.add_student(@student)
+      expect(@student.classroom).to eq(@classroom)
+    end
+  end
